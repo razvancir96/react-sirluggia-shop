99945ef7ecff40d465230c1f61e40c47002064cc
razvancir96 <>
2019-12-31 00:43:54 +0200

    prepares firebase for deploy

---
+++ src/configs/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCcbZMkwd4ZV_1DiH0R3TZFCg9GKaIMeoM",
+    authDomain: "react-sirluggia-shop.firebaseapp.com",
+    databaseURL: "https://react-sirluggia-shop.firebaseio.com",
+    projectId: "react-sirluggia-shop",
+    storageBucket: "react-sirluggia-shop.appspot.com",
+    messagingSenderId: "184253035461",
+    appId: "1:184253035461:web:20fe5c173c7675c74c51ac"
+};
+
+export default firebaseConfig;
